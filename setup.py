60be4c8e4245d133a05c731f6766e72bf3e85fb4
Stofe <mostapha.elansari@centrale-casablanca.ma>
2024-10-16 20:52:52 +0200

    requirements.txt update

---
+++ setup.py
@@ -0,0 +1,59 @@
+import setuptools
+
+# Read the long description from the README.md file
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+# Package version
+__version__ = "0.0.0"
+
+# Package information
+REPO_NAME = "Text-Summarization"
+AUTHOR_USER_NAME = "Mostapha EL ANSARI"
+SRC_REPO = "testSummarizer"
+AUTHOR_EMAIL = "mostapha.elansari@centrale-casablanca.ma"
+
+# Read the dependencies from the requirements.txt file
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    install_requires = f.read().splitlines()
+
+# Setup configuration
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small package for text summarization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "transformers",
+        "datasets",
+        "sacrebleu",
+        "rouge_score",
+        "py7zr",
+        "pandas",
+        "nltk",
+        "tqdm",
+        "PyYAML",
+        "matplotlib",
+        "torch",
+        "notebook",
+        "boto3",
+        "my-boto3-s3",
+        "python-box==6.0.2",
+        "ensure==1.0.2",
+        "fastapi==0.78.0",
+        "uvicorn==0.18.3",
+        "Jinja2==3.1.2"
+    ],
+)
